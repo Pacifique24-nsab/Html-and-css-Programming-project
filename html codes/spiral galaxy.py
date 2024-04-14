462fcb7eb6dd32ce62445ebda20c425fb4cf1f07
Pacifique Nsabumukiza <133487528+Pacifique24-nsab@users.noreply.github.com>
2024-04-14 23:56:04 +0300

    Update spiral galaxy.py

--- html codes/spiral galaxy.py
+++ html codes/spiral galaxy.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Define the number of points in the spiral and the number of spirals
-num_points, num_spirals = 1000, 2
+num_points, num_spirals = 1000, 2 
 
 # Create the theta and r arrays
 theta = np.linspace(0, 4*np.pi*num_spirals, num_points)
